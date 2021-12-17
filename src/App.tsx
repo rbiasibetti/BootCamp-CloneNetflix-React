62d3df76edc3cf30b1f1663b8190bb62ef5feb59
Rodrigo Biasibetti <rbiasibetti@outlook.com>
2021-12-16 22:05:27 -0300

    Criando CloneNetflix-React

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Login from './screens/login/login.screen';
+import { Route, Routes } from 'react-router-dom';
+import { LoginPath } from './screens/login/login.types';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path={LoginPath} element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }

ce602c2ab603623c1362fa829bdb0028dfbb6c07
Rodrigo Biasibetti <rbiasibetti@outlook.com>
2021-12-20 22:14:44 -0300

    Criando tema default

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Login from './screens/login/login.screen';
 import { Route, Routes } from 'react-router-dom';
 import { LoginPath } from './screens/login/login.types';
+import { ThemeProvider } from 'styled-components';
+import theme from './themes/main/theme';
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path={LoginPath} element={<Login />}></Route>
       </Routes>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,5 +1,14 @@
+import React from "react"
+import Button from "../../components/buttons/button/button.component"
+import { Grid } from '@mui/material'
 export default function Login() {
   return (
-    <h1>Teste</h1>
+    <>
+      <Grid container justifyContent={'center'}>
+        <Grid item xs={4}>
+          < Button primary={true}>Entrar</Button>
+        </Grid>
+      </Grid>
+    </>
   )
 }
